0aa84d220d45b98ce069eb4d929fd57dd7fc7f7d
martin <karlito40@gmail.com>
2018-08-14 14:10:43 +0200

    add lb4 test

---
+++ fofo-api/dist10/src/index.d.ts
@@ -0,0 +1,4 @@
+import { FofoApiApplication } from './application';
+import { ApplicationConfig } from '@loopback/core';
+export { FofoApiApplication };
+export declare function main(options?: ApplicationConfig): Promise<FofoApiApplication>;

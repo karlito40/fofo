2d46f7251d981fc153bcd5db97a22502d5a0e5c5
martin <karlito40@gmail.com>
2018-09-28 17:35:39 +0200

    fix import

--- fofo-web-ext/src/background/index.js
+++ fofo-web-ext/src/background/index.js
@@ -1,6 +1,5 @@
 import 'babel-polyfill';
 import './listeners/tab';
-import './listeners/external-message';
-import './listeners/internal-message';
+import './listeners/message';
 
 chrome.browserAction.setBadgeBackgroundColor({color: '#39dd92'});
